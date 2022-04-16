735bcc8def14cc614ee91aac132cd333a9fa461d
RickRollMaster101 <windowsrocks20@gmail.com>
2022-04-16 14:33:39 +0530

    Display img from the web instead of downloading it

--- Nitro_Gen_Hack.py
+++ Nitro_Gen_Hack.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtGui import QPixmap, QIcon, QImage
 import sys
 import os
 import webbrowser
@@ -12,25 +12,13 @@ from colorama import Fore, Back, Style
 
 colorama.init(autoreset=True)
 
-if os.path.exists('./nitro.png'):
-    pass
-else:
-    nitro = requests.get('https://github.com/RickRollMaster101/UselessCode/blob/main/stuff_for_useless_projects/nitro.png?raw=true')
-
-    open('./nitro.png', 'wb').write(nitro.content)
-
-if os.path.exists('./nitrologo.png'):
-    pass
-else:
-    nitrologo = requests.get('https://github.com/RickRollMaster101/UselessCode/blob/main/stuff_for_useless_projects/nitrologo.png?raw=true')
-
-    open('./nitrologo.png', 'wb').write(nitrologo.content)
+# nitrologo = requests.get('https://github.com/RickRollMaster101/UselessCode/blob/main/stuff_for_useless_projects/nitrologo.png?raw=true')
+# open('./nitrologo.png', 'wb').write(nitrologo.content)
 
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setWindowTitle('Nitro Gen Hack')
-        self.setWindowIcon(QIcon('nitro.png'))
         self.setGeometry(500, 500, 500, 250)
         self.initUI()
         
@@ -40,12 +28,16 @@ class Window(QMainWindow):
         self.label1.move(175, 115)
         self.label1.resize(200, 50)
 
-        self.label = QLabel(self)
-        self.pixmap = QPixmap('nitrologo.png')
-        self.label.setPixmap(self.pixmap)
-        self.label.resize(self.pixmap.width(),
-                          self.pixmap.height())
-        self.label.move(-65, -175)
+        image_url = 'https://github.com/RickRollMaster101/UselessCode/blob/main/stuff_for_useless_projects/nitrologo.png?raw=true'
+
+        self.image = QImage()
+        self.image.loadFromData(requests.get(image_url).content)
+
+        self.image_label = QLabel(self)
+        self.image_label.setPixmap(QPixmap.fromImage(self.image))
+        self.image_label.resize(self.image.width(),
+                                self.image.height())
+        self.image_label.move(-65, -175)
 
         self.button1 = QtWidgets.QPushButton(self)
         self.button1.setText('Exit')

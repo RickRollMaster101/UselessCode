5d0dd0213226e0392fced76986a25f52a68d26e4
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2022-05-17 12:09:45 +0530

    .

--- pyshell.py
+++ pyshell.py
@@ -28,6 +28,7 @@ if platform == 'linux':
     if os.path.exists(f'{linuxconfpath}/pyshell-conf.json'):
         pass
     else:
+        print(f'{Fore.YELLOW}[+] {Fore.RESET}No config file found, generating config file...')
         with open(f'{linuxconfpath}/pyshell-conf.json', 'a') as f:
             f.write(json.dumps(config))
 elif platform == 'darwin':
@@ -38,6 +39,7 @@ elif platform == 'darwin':
     if os.path.exists(f'{macconfpath}/pyshell-conf.json'):
         pass
     else:
+        print(f'{Fore.YELLOW}[+] {Fore.RESET}No config file found, generating config file...')
         with open(f'{macconfpath}/pyshell-conf.json', 'a') as f:
             f.write(json.dumps(config))
 
@@ -61,26 +63,26 @@ def main():
             try: 
                 print(os.listdir('.'))
             except:
-                print('ls: cannot access')
+                print(f'{Fore.RED}[x] {Fore.RESET}ls: cannot access')
         elif prompt == 'pwd':
             try:
                 print(os.getcwd())
             except:
-                print('pwd: cannot access')
+                print(f'{Fore.RED}[x] {Fore.RESET}pwd: cannot access')
         elif prompt == 'cd':
             try:
                 path = input('path: ')
                 os.chdir(path)
                 print(f'changed directory to {path}')
             except:
-                print('cd: folder not found')
+                print(f'{Fore.RED}[x] {Fore.RESET}cd: folder not found')
         elif prompt == 'cat':
             try:
                 filename = input('filename: ')
                 with open(filename, 'r') as f:
                     print(f.read())
             except:
-                print('cat: file not found')
+                print(f'{Fore.RED}[x] {Fore.RESET}cat: file not found')
         elif prompt == 'exit':
             print('exiting...')
             exit()
@@ -93,25 +95,25 @@ def main():
                 filename = input('filename: ')
                 open(filename, 'a').close()
             except:
-                print('touch: cannot create file')
+                print(f'{Fore.RED}[x] {Fore.RESET}touch: cannot create file')
         elif prompt == 'rm':
             try:
                 filename = input('filename: ')
                 os.remove(filename)
             except:
-                print('rm: cannot remove file')
+                print(f'{Fore.RED}[x] {Fore.RESET}rm: cannot remove file')
         elif prompt == 'mkdir':
             try:
                 dirname = input('dirname: ')
                 os.mkdir(dirname)
             except:
-                print('mkdir: cannot create folder')
+                print(f'{Fore.RED}[x] {Fore.RESET}mkdir: cannot create folder')
         elif prompt == 'rmdir':
             try:
                 dirname = input('dirname: ')
                 os.rmdir(dirname)
             except:
-                print('rmdir: cannot remove folder')
+                print(f'{Fore.RED}[x] {Fore.RESET}rmdir: cannot remove folder')
         elif prompt == 'bash':
             os.system('/bin/bash')
         elif prompt == 'help':
@@ -145,7 +147,7 @@ def main():
                 pycommand = input('command: ')
                 exec(pycommand)
             except:
-                print('python: invalid command')
+                print(f'{Fore.RED}[x] {Fore.RESET}python: invalid command')
         elif prompt == 'editconfig':
             if platform == 'linux':
                 os.system(f'nano {linuxconfpath}/pyshell-conf.json')
@@ -164,7 +166,7 @@ def main():
             if config['bashmode'] == 'on':
                 os.system(prompt)
             elif config['bashmode'] == 'off':
-                print(f'pyshell: command not found: {prompt} \n{Fore.YELLOW}If you want to run a bash command if the command doesnt exist in pyshell, set bashmode to "{Fore.GREEN}on{Fore.YELLOW}" in ~/.config/pyshell/pyshell-conf.json or by using the "editconfig" command')
+                print(f'{Fore.RED}[x] {Fore.RESET}pyshell: command not found: {prompt} \n{Fore.YELLOW}If you want to run a bash command if the command doesnt exist in pyshell, set bashmode to "{Fore.GREEN}on{Fore.YELLOW}" in ~/.config/pyshell/pyshell-conf.json or by using the "editconfig" command')
 
 if '__main__' == __name__:
     main()
